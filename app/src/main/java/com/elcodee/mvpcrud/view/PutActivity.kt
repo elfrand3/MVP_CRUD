126e64a27b345574873bd2d17e411523ad88d872
el_codee <elfrand3@gmail.com>
2023-02-14 06:56:33 +0700

    update

---
+++ app/src/main/java/com/elcodee/mvpcrud/view/PutActivity.kt
@@ -0,0 +1,12 @@
+package com.elcodee.mvpcrud.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elcodee.mvpcrud.R
+
+class PutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_put)
+    }
+}
